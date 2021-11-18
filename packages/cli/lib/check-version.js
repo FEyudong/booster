d8d7e0879d6c5d10cc5efc42832912297bf62040
zhengyudong <zhengyudong@58.com>
2021-11-18 23:31:53 +0800

    feat:注释

--- packages/cli/lib/check-version.js
+++ packages/cli/lib/check-version.js
@@ -1,4 +1,4 @@
-const request = require('request')
+// const request = require('request')
 const semver = require('semver')
 const chalk = require('chalk')
 const packageConfig = require('../package.json')
