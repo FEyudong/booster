488e2a3bc32be27604e95563419670ea0b199d0e
zhengyudong <zhengyudong@58.com>
2021-11-19 20:45:32 +0800

    chore:demo1

--- demo1/README.md
+++
@@ -1,2 +0,0 @@
-# build-react-app
-基于webpack配置的一款react脚手架，集成了typecript、eslint等常见工具
--- demo1/proxyTable.js
+++
@@ -1,7 +0,0 @@
-// 这里配置代理
-module.exports = {
-    '/api': {
-        target: 'http://localhost:3000',
-        pathRewrite: { '^/api': '' },
-    },
-};
